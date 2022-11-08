e221d37b04831d2eb15ec2f3ecf94ef4b7982236
jaferIdrees <jaferll@gmail.com>
2022-11-08 21:53:22 +0300

    Add recipe partial

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,13 @@
 class RecipesController < ApplicationController
   def index
+    @user = current_user
     @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
   end
 end
