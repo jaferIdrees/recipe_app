da748f42a9befe578d78846dd49215839e502bf1
jaferIdrees <jaferll@gmail.com>
2022-11-09 00:07:02 +0300

    fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,13 +10,15 @@ class RecipesController < ApplicationController
 
   def create
     new_recipe = Recipe.new(user: current_user, name: params[:recipe][:name],
-   preparation_time: params[:recipe][:preparation_time], cooking_time: params[:recipe][:cooking_time], description: params[:recipe][:description],
-   public: params[:recipe][:public])
-   if new_recipe.save
-    render inline: "<p>Recipe saved successfuly.</p>"
-   else
-    render inline: "<p>Error</p>"
-   end
+                            preparation_time: params[:recipe][:preparation_time],
+                            cooking_time: params[:recipe][:cooking_time],
+                            description: params[:recipe][:description],
+                            public: params[:recipe][:public])
+    if new_recipe.save
+      render inline: '<p>Recipe saved successfuly.</p>'
+    else
+      render inline: '<p>Error</p>'
+    end
   end
 
   def destroy
