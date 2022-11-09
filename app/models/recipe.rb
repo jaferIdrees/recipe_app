06d13f7446b2277e8e86cc685b648f54ad3fc3c4
jaferIdrees <jaferll@gmail.com>
2022-11-09 16:36:26 +0300

    resolve conflict

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,4 +10,6 @@ class Recipe < ApplicationRecord
   def total_price
     5 # this should be implemnted from new assocation
   end
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :preperation_time, :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
