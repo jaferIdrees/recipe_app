768062b46d71d3a145d4d4074f7158f384660f89
jaferIdrees <jaferll@gmail.com>
2022-11-09 16:36:26 +0300

    resolve conflict

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :preperation_time, :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@
 class User < ApplicationRecord
   has_many :foods
   has_many :recipes
+
+  validates :name, presence: true
 end
